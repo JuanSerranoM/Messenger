82e7333b9e464d2b80b639ecf33f73cadadf056e
Juan <Juan>
2017-03-14 00:39:43 -0300

    cambio

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ const express = require('express'),
       bodyParser = require ('body-parser'),
       request = require('request');
 
-const 
+const APP_TOKEN = '';
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.listen(3000,function(){
+    console.log('Server listen localhost: 3000');
+});
